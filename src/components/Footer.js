5a3a53ee980fa24f04e5737c675efd8a0af1f7d3
n2jato00 <n2jato00@students.oamk.fi>
2023-02-26 20:34:09 +0200

    Components for site

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span>Place sticky footer content here.</span>
+      </div>
+    </footer>
+  )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/Header.css'
 
 export default function Header() {
     return (
-        <div>
-            <h1>Meme site</h1>
+        <div id="header">
+            <h1>genderize.io</h1>
         </div>
     )
 }
