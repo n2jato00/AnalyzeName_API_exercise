c4c30730e258a1c3aecdcb6ae4477fccdf22661f
n2jato00 <n2jato00@students.oamk.fi>
2023-02-26 20:36:30 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,63 +1,29 @@
 import './App.css';
-import './styles/Meme.css'
-import './styles/Header.css'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Meme from './components/Meme';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
-
-
-const URL = "https://api.imgflip.com/get_memes"
+const URL = "https://api.imgflip.com/"
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
-
-  useEffect(() => {
-
-    axios.get(URL)
-      .then((response) => {
-        setError(null)
-        setIsLoaded(true)
-        setItems(response.data.data.memes)
-      })
-      .catch(error => {
-        alert(error);
-      });
-  }, []);
-
-  function close() {
-    setSelectedItem(null)
-  }
-
-  if (selectedItem != null) {
-    return <Meme
-      title={selectedItem.name}
-      image={selectedItem.url}
-      close={close}
-    />
-  }
-  else if (error) {
-    return <p>{error.message}</p>
-  }
-  else if (!isLoaded) {
-    return <p>Loading...</p>
-  }
-  else {
-    return (
-      <div>
-        <Header />
-        {items?.map(item => (
-          <div key={item.name} onClick={e => setSelectedItem(item)}>
-            <img src={item.url} />
-          </div>
-        ))}
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-    );
-  }
+      <Footer />
+    </>
+  );
 }
 
 export default App;
