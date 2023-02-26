74bfa364dafcebcb7d0e9f34c512df0278a9720c
n2jato00 <n2jato00@students.oamk.fi>
2023-02-26 20:35:26 +0200

    about us site

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h2>Determine the gender of a name</h2>
+      <p>
+        A simple API to predict the gender of a person given their name
+        The API is free for up to 1000 names/day. No sign up or API key needed. So go ahead and try it out.
+      </p>
+    </div>
+  )
+}
